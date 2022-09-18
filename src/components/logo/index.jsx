f1e90f881ba62cac25145c9796dbf38b376d9c79
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-09-18 12:01:29 +0530

    Update index.jsx

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo-ax.png';
+import logo from '../../assets/images/logo.png';
 
 // ----------------------------------------------------------------------
 
