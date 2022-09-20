3f92a1a64eb4c68739aeaf69e9905082b1fd22f3
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-09-20 15:27:21 +0530

    Update embedApi.js

--- src/api/embedApi.js
+++ src/api/embedApi.js
@@ -1,12 +1,12 @@
-// import { EMBED_URL } from '../config';
+import { EMBED_URL } from '../config';
 
-// ----------------------------------------------------------------------
+----------------------------------------------------------------------
 
-// const embedMovie = (id) => `${EMBED_URL}/${id}`;
-// const embedEpisode = (id, season, episode) =>
-//   `${EMBED_URL}/${id}/${season}/${episode}`;
-   const embedMovie = (id) => `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
-   const embedEpisode =  (id, season, episode) =>
-    `https://www.2embed.to/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
+const embedMovie = (id) => `${EMBED_URL}/${id}`;
+const embedEpisode = (id, season, episode) =>
+  `${EMBED_URL}/${id}/${season}/${episode}`;
+//    const embedMovie = (id) => `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
+//    const embedEpisode =  (id, season, episode) =>
+//     `https://www.2embed.to/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
 
 export { embedMovie, embedEpisode };
